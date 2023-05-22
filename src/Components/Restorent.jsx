86c10d254f4e80036647a9f5c63ce335bbe09890
Pankaj Kumar Verma <pankajpatel916174@gmail.com>
2023-05-22 22:40:22 +0530

    Restorent web

---
+++ src/Components/Restorent.jsx
@@ -0,0 +1,12 @@
+//  import React from 'react';
+ import './Restorent.css';
+
+const Restorent = () => {
+  return (
+    <> 
+        <h1>Hello resturant</h1>
+    </>
+  )
+}
+
+export default Restorent
